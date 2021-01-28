00484b8f7198c60a3fe875e258cec1581ba7bf7d
rainbowmoonlight <46021863+rainbowmoonlight@users.noreply.github.com>
2021-01-28 01:00:12 -0800

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from ml_app import run_ml_app
 
 html_temp = """
 		<div style="background-color:#3872fb;padding:10px;border-radius:10px">
-		<h1 style="color:white;text-align:center;">Early Stage DM Risk Data App </h1>
+		<h1 style="color:white;text-align:center;">Early Stage Diabetes Risk Data App </h1>
 		<h4 style="color:white;text-align:center;">Diabetes </h4>
 		</div>
 		"""
